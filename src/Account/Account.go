6bc076dcc361273b9050851583865020cc7ca710
antegral <antegral@antegral.net>
2022-10-22 12:06:36 +0900

    Update AwsCredential type (Add BucketPrefix)

--- src/Account/Account.go
+++ src/Account/Account.go
@@ -6,16 +6,17 @@ import (
 )
 
 type AwsCredential struct {
-	Region     string
-	ApiId      string
-	ApiKey     string
-	S3BucketId string
+	Region       string
+	ApiId        string
+	ApiKey       string
+	BucketId     string
+	BucketPrefix string
 }
 
-func GetB64Hash(plain string) string {
+func GetB64Hash(Plain string) string {
 	sha := sha512.New()
 	sha.Write([]byte("SALT+D3ntg902md+"))
-	sha.Write([]byte(plain))
+	sha.Write([]byte(Plain))
 	b64hash := b64.StdEncoding.EncodeToString(sha.Sum(nil))
 	return b64hash
 }
