a60cb62fde9c62cfc2c3dc600f298304f6890951
antegral <antegral@antegral.net>
2022-10-23 13:45:23 +0900

    Remove TestUseLogFile

--- src/Log/Log_test.go
+++ src/Log/Log_test.go
@@ -1,25 +1,13 @@
 package Log
 
 import (
-	"os"
 	"testing"
 )
 
 func TestLogInit(t *testing.T) {
-	Init(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+	Init()
 	Verbose.Println("Test successful.")
 	Info.Println("Test successful.")
 	Warn.Println("Test successful.")
 	Error.Println("Test successful.")
-}
-
-func TestUseLogFile(t *testing.T) {
-	if err := InitLogFile(); err != nil {
-		Error.Println(err)
-	}
-
-	Verbose.Println("Test successful.")
-	Info.Println("Test successful.")
-	Warn.Println("Test successful.")
-	Error.Println("Test successful")
 }
