227373e242bf1fa1decb9a209865244b8bb394dc
antegral <antegral@antegral.net>
2022-11-01 12:07:22 +0900

    Add LoginStrategy()

--- src/Account/Account.go
+++ src/Account/Account.go
@@ -3,6 +3,9 @@ package Account
 import (
 	"crypto/sha512"
 	b64 "encoding/base64"
+
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/backend"
 )
 
 type AwsCredential struct {
@@ -19,4 +22,8 @@ func GetB64Hash(Plain string) string {
 	sha.Write([]byte(Plain))
 	b64hash := b64.StdEncoding.EncodeToString(sha.Sum(nil))
 	return b64hash
+}
+
+func LoginStrategy(connInfo *imap.ConnInfo, username, password string) (backend.User, error) {
+	return nil, nil
 }
