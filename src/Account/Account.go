fa7c0ad2213d51ef6436ffafb3c53fbd43acf018
antegral <antegral@antegral.net>
2022-11-10 21:44:20 +0900

    Add: Partial backend.Mailbox Implementation (struct Mailbox) Change: PostgreSQL supports UUID type, does not use string conversion

--- src/Account/Account.go
+++ src/Account/Account.go
@@ -3,6 +3,7 @@ package Account
 import (
 	"context"
 	"crypto/subtle"
+	"time"
 
 	"antegral.net/mailmix/src/Database"
 	ORM "antegral.net/mailmix/src/Database/Sqlc"
@@ -45,7 +46,7 @@ func LoginStrategy(connInfo *imap.ConnInfo, username, password string) (backend.
 
 	SessionUuid := uuid.New()
 	_, err = db.CreateSession(ctx, ORM.CreateSessionParams{
-		Uuid:        SessionUuid.String(),
+		Uuid:        SessionUuid,
 		Accountuuid: UserData.Uuid,
 	})
 
@@ -71,13 +72,13 @@ func (User ImapUser) Username() string {
 }
 
 func (User ImapUser) ListMailboxes(subscribed bool) ([]backend.Mailbox, error) {
-	// ctx := context.Background()
+	ctx := context.Background()
 
-	// // TODO: Mailbox
-	// _, err := User.Query.GetAllMailBoxInfo(ctx, User.Data.Uuid)
-	// if err != nil {
-	// 	return nil, backend.ErrInvalidCredentials
-	// }
+	// TODO: Mailbox
+	_, err := User.Query.GetAllMailBoxInfo(ctx, User.Data.Uuid)
+	if err != nil {
+		return nil, backend.ErrInvalidCredentials
+	}
 
 	return nil, nil
 }
@@ -90,7 +91,7 @@ func (User ImapUser) CreateMailbox(name string) error {
 	ctx := context.Background()
 
 	_, err := User.Query.CreateMailBox(ctx, ORM.CreateMailBoxParams{
-		Uuid:      uuid.New().String(),
+		Uuid:      uuid.New(),
 		Name:      name,
 		Owneruuid: User.Data.Uuid,
 	})
@@ -163,5 +164,57 @@ func (User ImapUser) Logout() error {
 		return backend.ErrInvalidCredentials
 	}
 
+	return nil
+}
+
+type Mailbox struct {
+	Owner       ImapUser
+	MailboxName string
+}
+
+func (Info Mailbox) Name() string {
+	return Info.MailboxName
+}
+
+func (User Mailbox) Info() (*imap.MailboxInfo, error) {
+	// Mailbox Attributes (https://www.iana.org/assignments/imap-mailbox-name-attributes/imap-mailbox-name-attributes.xhtml)
+	Info := imap.MailboxInfo{}
+	return &Info, nil
+}
+
+func (User Mailbox) Status(items []imap.StatusItem) (*imap.MailboxStatus, error) {
+	return nil, nil
+}
+
+func (User Mailbox) SetSubscribed(subscribed bool) error {
+	return nil
+}
+
+func (User Mailbox) Check() error {
+	// TODO: housekeeping considerations
+	return nil
+}
+
+func (User Mailbox) ListMessages(uid bool, seqset *imap.SeqSet, items []imap.FetchItem, ch chan<- *imap.Message) error {
+	return nil
+}
+
+func (User Mailbox) SearchMessages(uid bool, criteria *imap.SearchCriteria) ([]uint32, error) {
+	return nil, nil
+}
+
+func (User Mailbox) CreateMessage(flags []string, date time.Time, body imap.Literal) error {
+	return nil
+}
+
+func (User Mailbox) UpdateMessagesFlags(uid bool, seqset *imap.SeqSet, operation imap.FlagsOp, flags []string) error {
+	return nil
+}
+
+func (User Mailbox) CopyMessages(uid bool, seqset *imap.SeqSet, dest string) error {
+	return nil
+}
+
+func (User Mailbox) Expunge() error {
 	return nil
 }
