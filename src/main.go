efb446946ff2e759fc217ebb04b8057a01bd134a
antegral <antegral@antegral.net>
2022-11-10 21:40:14 +0900

    Add: default initialization logic

--- src/main.go
+++ src/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+
+	DB "antegral.net/mailmix/src/Database"
+	"antegral.net/mailmix/src/EnvCheck"
 	"antegral.net/mailmix/src/Log"
 )
 
@@ -9,5 +13,40 @@ func main() {
 		panic(err)
 	}
 
-	Log.Info.Println("Starting MailMix...")
+	Log.Info.Println("MailMix IMAP Server")
+	Log.Info.Println("Version: at-mm:0.1:221110112439")
+	Log.Info.Println("Checking Environment Values...")
+
+	FilePath, err := EnvCheck.GetEnvFilePath()
+	Log.Info.Print("Searching: ", FilePath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if !EnvCheck.IsFileExists(FilePath) {
+		Log.Info.Print("Not found ENV File. Creating File...")
+		_, err = os.Create(FilePath)
+		if err != nil {
+			panic(err)
+		}
+		EnvCheck.Run()
+		Log.Info.Print("ENV File Created. Please set up Environment Values. File Path: ", FilePath)
+		os.Exit(1)
+	}
+
+	Log.Info.Println("Initializing Database...")
+
+	err = DB.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	Log.Info.Println("Loading Database...")
+	_, err = DB.GetDatabase()
+	if err != nil {
+		panic(err)
+	}
+
+	Log.Info.Println("Database Initialized.")
 }
