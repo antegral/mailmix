8b81fd1b888c6d8d05b3ec578130d4be268d5605
antegral <antegral@antegral.net>
2022-11-10 21:40:57 +0900

    Remove: Temporarily remove test code to be rewritten after future implementation

--- src/Account/Account_test.go
+++ src/Account/Account_test.go
@@ -1,14 +1,1 @@
 package Account
-
-import (
-	"testing"
-)
-
-func TestGetB64Hash(t *testing.T) {
-	VaildHash := "r/FgKFuU0VX7JLueIQItPqgW4M1gcjcrIf4ninYUMNT2ct2LuNDwbfwmfdhqSkP/vQ5sEzIVHWXsYLif59KeYA=="
-	Got := GetB64Hash("mailmix")
-
-	if Got != VaildHash {
-		t.Errorf("TestHash > invaild hash")
-	}
-}
