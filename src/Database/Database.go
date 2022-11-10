b70280bcb84ab4b36fbca3fc9e7814096c129f6e
antegral <antegral@antegral.net>
2022-11-10 21:38:55 +0900

    Add: Import ConnString as an environment variable Remove: Remove GetDatabasePath() because it is no longer using the local database Change: DB driver to postgresql Change: do not use createTableQuery During Database initialize

--- src/Database/Database.go
+++ src/Database/Database.go
@@ -2,89 +2,15 @@ package Database
 
 import (
 	"database/sql"
-	"fmt"
 	"os"
 
-	"github.com/google/uuid"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
 )
 
-var createTableQuery = `
-CREATE TABLE IF NOT EXISTS AwsCredential (
-	Uuid			TEXT NOT NULL,
-	Region			TEXT NOT NULL,
-	Id				TEXT NOT NULL UNIQUE,
-	Secret			TEXT NOT NULL,
-	BucketId		TEXT NOT NULL,
-	BucketPrefix	TEXT,
-	CreatedAt		TIMESTEMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY(Uuid)
-);
-
-CREATE TABLE IF NOT EXISTS Team (
-	Uuid			TEXT NOT NULL,
-	Name			TEXT NOT NULL UNIQUE,
-	Description		TEXT,
-	CredentialUuid	TEXT NOT NULL UNIQUE,
-	CreatedAt		TIMESTEMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY(Uuid),
-	FOREIGN KEY(CredentialUuid) REFERENCES AwsCredential(Uuid) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS Account (
-	Uuid		TEXT NOT NULL,
-	TeamUuid	TEXT,
-	Username	TEXT NOT NULL UNIQUE,
-	Password	TEXT NOT NULL,
-	MailAddress	TEXT NOT NULL UNIQUE,
-	IsQuit		INTEGER NOT NULL DEFAULT 0,
-	CreatedAt	TIMESTEMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY(Uuid),
-	FOREIGN KEY(TeamUuid) REFERENCES Team(Uuid) ON DELETE SET NULL ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS MailBox (
-	Uuid		TEXT NOT NULL,
-	Name		TEXT NOT NULL,
-	OwnerUuid	TEXT NOT NULL,
-	Header      TEXT NOT NULL,
-	Size        INTEGER NOT NULL,
-	SentAt      TEXT NOT NULL,
-	CreatedAt	TIMESTEMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY(Uuid)
-);
-
-CREATE TABLE Mail (
-	Uuid		TEXT NOT NULL,
-  BoxUuid		TEXT NOT NULL,
-	CreatedAt	TIMESTEMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY(Uuid)
-);
-
-CREATE TABLE IF NOT EXISTS Session (
-	Uuid        TEXT NOT NULL,
-	AccountUuid TEXT NOT NULL,
-	CreatedAt	TIMESTEMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY(Uuid)
-);
-`
-
 func Init() error {
-	DBFilePath, err := GetDatabasePath()
+	_, err := GetDatabase()
 	if err != nil {
-		return err
-	}
-
-	if _, err := os.Create(DBFilePath); err != nil {
-		return err
-	}
-
-	Database, err := GetDatabase()
-	if err != nil {
-		return err
-	}
-
-	if _, err = Database.Exec(createTableQuery); err != nil {
 		return err
 	}
 
@@ -92,28 +18,11 @@ func Init() error {
 }
 
 func GetDatabase() (*sql.DB, error) {
-	DBFilePath, err := GetDatabasePath()
-	if err != nil {
-		return nil, err
-	}
+	ConnString := os.Getenv("MAILMIX_DATABASE_CONNSTRING")
 
-	db, err := sql.Open("sqlite3", DBFilePath)
+	db, err := sql.Open("postgres", ConnString)
 	if err != nil {
 		return nil, err
 	}
 	return db, err
-}
-
-func GetDatabasePath() (string, error) {
-	// 현재 경로 가져오기
-	pwd, err := os.Getwd()
-	if err != nil {
-		return "", err // os.Getwd() 예외처리
-	}
-
-	// 경로 포맷팅
-	return fmt.Sprint(pwd, "/database.db"), nil
-}
-
-func GetUser(Uuid uuid.UUID) {
 }
