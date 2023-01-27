b87f52c4f6753eebc927974913f4246825cb8ee3
antegral <antegral@antegral.net>
2023-01-28 01:02:55 +0900

    Change: simplify method's receiver name

--- src/Backend/ImapUser.go
+++ src/Backend/ImapUser.go
@@ -14,15 +14,15 @@ type ImapUser struct {
 	Query   *ORM.Queries
 }
 
-func (User *ImapUser) Username() string {
-	return User.Data.Username
+func (u *ImapUser) Username() string {
+	return u.Data.Username
 }
 
-func (User *ImapUser) ListMailboxes(subscribed bool) ([]backend.Mailbox, error) {
+func (u *ImapUser) ListMailboxes(subscribed bool) ([]backend.Mailbox, error) {
 	ctx := context.Background()
 
 	// TODO: Mailbox (Mailbox implementation: /src/Backend/Mailbox.go)
-	_, err := User.Query.GetAllMailBoxInfo(ctx, User.Data.Uuid)
+	_, err := u.Query.GetAllMailBoxInfo(ctx, u.Data.Uuid)
 	if err != nil {
 		return nil, backend.ErrInvalidCredentials
 	}
@@ -30,17 +30,17 @@ func (User *ImapUser) ListMailboxes(subscribed bool) ([]backend.Mailbox, error) 
 	return nil, nil
 }
 
-func (User *ImapUser) GetMailbox(name string) (backend.Mailbox, error) {
+func (u *ImapUser) GetMailbox(name string) (backend.Mailbox, error) {
 	return nil, nil
 }
 
-func (User *ImapUser) CreateMailbox(name string) error {
+func (u *ImapUser) CreateMailbox(name string) error {
 	ctx := context.Background()
 
-	_, err := User.Query.CreateMailBox(ctx, ORM.CreateMailBoxParams{
+	_, err := u.Query.CreateMailBox(ctx, ORM.CreateMailBoxParams{
 		Uuid:      uuid.New(),
 		Name:      name,
-		Owneruuid: User.Data.Uuid,
+		Owneruuid: u.Data.Uuid,
 	})
 	if err != nil {
 		return backend.ErrInvalidCredentials
@@ -49,25 +49,25 @@ func (User *ImapUser) CreateMailbox(name string) error {
 	return nil
 }
 
-func (User *ImapUser) DeleteMailbox(name string) error {
+func (u *ImapUser) DeleteMailbox(name string) error {
 	ctx := context.Background()
 
-	MailBox, err := User.Query.GetOneMailBoxInfo(ctx, ORM.GetOneMailBoxInfoParams{
+	MailBox, err := u.Query.GetOneMailBoxInfo(ctx, ORM.GetOneMailBoxInfoParams{
 		Name:      name,
-		Owneruuid: User.Data.Uuid,
+		Owneruuid: u.Data.Uuid,
 	})
 	if err != nil {
 		return backend.ErrNoSuchMailbox
 	}
 
-	err = User.Query.DeleteAllMailInMailBox(ctx, MailBox.Uuid)
+	err = u.Query.DeleteAllMailInMailBox(ctx, MailBox.Uuid)
 	if err != nil {
 		return backend.ErrNoSuchMailbox
 	}
 
-	err = User.Query.DeleteMailBox(ctx, ORM.DeleteMailBoxParams{
+	err = u.Query.DeleteMailBox(ctx, ORM.DeleteMailBoxParams{
 		Name:      name,
-		Owneruuid: User.Data.Uuid,
+		Owneruuid: u.Data.Uuid,
 	})
 	if err != nil {
 		return backend.ErrNoSuchMailbox
@@ -76,12 +76,12 @@ func (User *ImapUser) DeleteMailbox(name string) error {
 	return nil
 }
 
-func (User *ImapUser) RenameMailbox(existingName, newName string) error {
+func (u *ImapUser) RenameMailbox(existingName, newName string) error {
 	ctx := context.Background()
 
-	Count, err := User.Query.CountUserOwnedMailBox(ctx, ORM.CountUserOwnedMailBoxParams{
+	Count, err := u.Query.CountUserOwnedMailBox(ctx, ORM.CountUserOwnedMailBoxParams{
 		Name:      newName,
-		Owneruuid: User.Data.Uuid,
+		Owneruuid: u.Data.Uuid,
 	})
 	if err != nil {
 		return backend.ErrNoSuchMailbox
@@ -91,10 +91,10 @@ func (User *ImapUser) RenameMailbox(existingName, newName string) error {
 		return backend.ErrMailboxAlreadyExists
 	}
 
-	err = User.Query.RenameMailBox(ctx, ORM.RenameMailBoxParams{
+	err = u.Query.RenameMailBox(ctx, ORM.RenameMailBoxParams{
 		Name:      newName,
 		Name_2:    existingName,
-		Owneruuid: User.Data.Uuid,
+		Owneruuid: u.Data.Uuid,
 	})
 	if err != nil {
 		return backend.ErrNoSuchMailbox
@@ -103,10 +103,10 @@ func (User *ImapUser) RenameMailbox(existingName, newName string) error {
 	return nil
 }
 
-func (User *ImapUser) Logout() error {
+func (u *ImapUser) Logout() error {
 	ctx := context.Background()
 
-	err := User.Query.DeleteSessionByUser(ctx, User.Data.Uuid)
+	err := u.Query.DeleteSessionByUser(ctx, u.Data.Uuid)
 	if err != nil {
 		return backend.ErrInvalidCredentials
 	}
